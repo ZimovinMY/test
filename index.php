31ed068c5649e03308d44c1bafa62a39564080d0
ZimovinMY <ZimovinMY@mpei.ru>
2024-06-01 19:15:19 +0300

    final

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration Form</title>
+    <title>Registration</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <style>
         .error { color: red; }
@@ -54,7 +54,6 @@
                 url: "register.php",
                 data: formData,
                 dataType: "json",
-                encode: true,
                 success: function(response){
                     console.log(response);
                     if(response.success){
